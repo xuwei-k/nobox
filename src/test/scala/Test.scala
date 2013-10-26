4628975776760c307360e7718af26b883d6d1554
xuwei-k <6b656e6a69@gmail.com>
2013-10-26 21:08:34 +0900

    improve test

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -7,7 +7,7 @@ object Test extends Properties("nobox"){
   def fail(message: String) =
     throw new AssertionError(message)
 
-  implicit class AnyOps(actual: => Any) {
+  implicit class AnyOps[A](actual: => A) {
     def mustThrowA[T <: Throwable](implicit man: reflect.ClassTag[T]): Boolean = {
       val erasedClass = man.runtimeClass
       try {
@@ -22,7 +22,7 @@ object Test extends Properties("nobox"){
       }
     }
 
-    def must_==(that: Any): Boolean = {
+    def must_==(that: A): Boolean = {
       val self = actual
       if(self == that) true
       else {
@@ -32,7 +32,7 @@ object Test extends Properties("nobox"){
   }
 
   implicit class ArrayOps[A](val self: Array[A]) extends AnyVal {
-    def ===(that: Array[A]): Boolean = {
+    def must_===(that: Array[A]): Boolean = {
       if(self sameElements that) true
       else {
         val msg = self.mkString("Array(",",",")") + " is not equal " + that.mkString("Array(",",",")")
@@ -51,43 +51,43 @@ object Test extends Properties("nobox"){
   val f: Int => Boolean = {i: Int => 5 < i && i < 10 }
 
   property("collect") = forAll { a: ofInt =>
-    a.collectLong(pf).self === a.self.collect(pf)
+    a.collectLong(pf).self must_=== a.self.collect(pf)
   }
 
   property("collectFirst") = forAll { a: ofInt =>
-    a.collectFirstLong(pf) == a.self.collectFirst(pf)
+    a.collectFirstLong(pf) must_== a.self.collectFirst(pf)
   }
 
   property("exists") = forAll { a: ofInt =>
-    a.exists(f) == a.self.exists(f)
+    a.exists(f) must_== a.self.exists(f)
   }
 
   property("filter") = forAll { a: ofInt =>
-    a.filter(f).self === a.self.filter(f)
+    a.filter(f).self must_=== a.self.filter(f)
   }
 
   property("find") = forAll { a: ofInt =>
-    a.find(f) == a.self.find(f)
+    a.find(f) must_== a.self.find(f)
   }
 
   property("flatMap") = forAll { a: ofInt =>
     val f: Int => Array[Int] = {i: Int => Array(i, i + 10)}
-    a.flatMapInt(f).self === a.self.flatMap(x => f(x).toList)
+    a.flatMapInt(f).self must_=== a.self.flatMap(x => f(x).toList)
   }
 
   property("forall") = forAll { a: ofInt =>
     val f = {i: Int => 5 < i }
-    a.forall(f) == a.self.forall(f)
+    a.forall(f) must_== a.self.forall(f)
   }
 
   property("map") = forAll { a: ofInt =>
     val f = {i: Int => i - 1 }
-    a.mapInt(f).self === a.self.map(f)
+    a.mapInt(f).self must_=== a.self.map(f)
   }
 
   property("reverseMap") = forAll { a: ofInt =>
     val f = {i: Int => i * 2 }
-    a.reverseMapInt(f).self === a.self.reverseMap(f)
+    a.reverseMapInt(f).self must_=== a.self.reverseMap(f)
   }
 
   property("reverse.reverse") = forAll { a: ofInt =>
@@ -95,92 +95,92 @@ object Test extends Properties("nobox"){
   }
 
   property("reverse") = forAll { a: ofInt =>
-    a.reverse.self === a.self.reverse
+    a.reverse.self must_=== a.self.reverse
   }
 
   property("take") = forAll { (a: ofInt, n: Int) =>
-    a.take(n).self === a.self.take(n)
+    a.take(n).self must_=== a.self.take(n)
   }
 
   property("takeWhile") = forAll { a: ofInt =>
     val f = {i: Int => 5 < i }
-    a.takeWhile(f).self === a.self.takeWhile(f)
+    a.takeWhile(f).self must_=== a.self.takeWhile(f)
   }
 
   property("takeRight") = forAll { (a: ofInt, n: Int) =>
-    a.takeRight(n).self === a.self.toList.takeRight(n).toArray
+    a.takeRight(n).self must_=== a.self.toList.takeRight(n).toArray
   }
 
   property("count") = forAll { a: ofInt =>
-    a.count(f) == a.self.count(f)
+    a.count(f) must_== a.self.count(f)
   }
 
   property("drop") = forAll { (a: ofInt, n: Int) =>
-    a.drop(n).self === a.self.drop(n)
+    a.drop(n).self must_=== a.self.drop(n)
   }
 
   property("dropWhile") = forAll { a: ofInt =>
     val f = {i: Int => i > 3}
-    a.dropWhile(f).self === a.self.dropWhile(f)
+    a.dropWhile(f).self must_=== a.self.dropWhile(f)
   }
 
   property("dropRight") = forAll { (a: ofInt, n: Int) =>
-    a.dropRight(n).self === a.self.toList.dropRight(n).toArray
+    a.dropRight(n).self must_=== a.self.toList.dropRight(n).toArray
   }
 
   property("contains") = forAll { (a: ofInt, n: Int) =>
-    a.contains(n) == a.self.contains(n)
+    a.contains(n) must_== a.self.contains(n)
   }
 
   property("splitAt") = forAll { (a: ofInt, n: Int) =>
     val (x1, x2) = a.splitAt(n)
     val (y1, y2) = a.self.splitAt(n)
-    (x1.self === y1) && (x2.self === y2)
+    (x1.self must_=== y1) && (x2.self must_=== y2)
   }
 
   property("span") = forAll { a: ofInt =>
     val f = {i: Int => i > 0}
     val (x1, x2) = a.span(f)
     val (y1, y2) = a.self.span(f)
-    (x1.self === y1) && (x2.self === y2)
+    (x1.self must_=== y1) && (x2.self must_=== y2)
   }
 
   property("++") = forAll { (a: ofInt, b: ofInt) =>
-    (a ++ b).self === (a.self ++ b.self)
+    (a ++ b).self must_=== (a.self ++ b.self)
   }
 
   property("partition") = forAll { a: ofInt =>
     val f = {i: Int => i > 0}
     val (x1, x2) = a.partition(f)
     val (y1, y2) = a.self.partition(f)
-    (x1.self === y1) && (x2.self === y2)
+    (x1.self must_=== y1) && (x2.self must_=== y2)
   }
 
   property("reverse_:::") = forAll { (a: ofInt, b: ofInt) =>
-    (a reverse_::: b).self === (a.self.toList reverse_::: b.self.toList).toArray
+    (a reverse_::: b).self must_=== (a.self.toList reverse_::: b.self.toList).toArray
   }
 
   property("updated") = forAll { (a: ofInt, index: Int, elem: Int) =>
     if(0 <= index && index < a.size)
-      a.updated(index, elem).self === a.self.updated(index, elem)
+      a.updated(index, elem).self must_=== a.self.updated(index, elem)
     else
       a.updated(index, elem).mustThrowA[IndexOutOfBoundsException]
   }
 
   property("sum ofInt") = forAll { a: ofInt =>
-    a.sum == a.self.sum
+    a.sum must_== a.self.sum
   }
 
   property("sum ofFloat") = forAll { a: ofFloat =>
-    a.sum == a.self.sum
+    a.sum must_== a.self.sum
   }
 
   property("sumLong") = forAll { a: ofInt =>
-    a.sumLong == a.self.map(_.toLong).sum
+    a.sumLong must_== a.self.map(_.toLong).sum
   }
 
   property("product ofInt") = forAll { a: ofInt =>
-    a.product == a.self.product
+    a.product must_== a.self.product
   }
 
   property("product ofFloat") = forAll { a: ofFloat =>
@@ -190,7 +190,7 @@ object Test extends Properties("nobox"){
   }
 
   property("productLong") = forAll { a: ofInt =>
-    a.productLong == a.self.map(_.toLong).product
+    a.productLong must_== a.self.map(_.toLong).product
   }
 
   property("productDouble") = forAll { a: ofInt =>
@@ -200,55 +200,55 @@ object Test extends Properties("nobox"){
   }
 
   property("sorted") = forAll { a: ofFloat =>
-    a.sorted.self === a.self.sorted
+    a.sorted.self must_=== a.self.sorted
   }
 
   property("slice") = forAll { (a: ofInt, from: Int, until: Int) =>
-    a.slice(from, until).self === a.self.slice(from, until)
+    a.slice(from, until).self must_=== a.self.slice(from, until)
   }
 
   property("reduceLeftOption") = forAll { a: ofInt =>
-    a.reduceLeftOption(_ - _) == a.self.reduceLeftOption(_ - _)
+    a.reduceLeftOption(_ - _) must_== a.self.reduceLeftOption(_ - _)
   }
 
   property("reduceRightOption") = forAll { a: ofInt =>
-    a.reduceRightOption(_ - _) == a.self.reduceRightOption(_ - _)
+    a.reduceRightOption(_ - _) must_== a.self.reduceRightOption(_ - _)
   }
 
   property("foldLeftInt") = forAll { (a: ofInt, z: Int) =>
-    a.foldLeftInt(z)(_ - _) == a.self.foldLeft(z)(_ - _)
+    a.foldLeftInt(z)(_ - _) must_== a.self.foldLeft(z)(_ - _)
   }
 
   property("foldLeft") = forAll { (a: ofInt, z: List[Int]) =>
-    a.foldLeft(z.toVector)(_ :+ _) == a.self.foldLeft(z.toVector)(_ :+ _)
+    a.foldLeft(z.toVector)(_ :+ _) must_== a.self.foldLeft(z.toVector)(_ :+ _)
   }
 
   property("foldRightLong") = forAll { (a: ofInt, z: Long) =>
-    a.foldRightLong(z)(_ - _) == a.self.foldRight(z)(_ - _)
+    a.foldRightLong(z)(_ - _) must_== a.self.foldRight(z)(_ - _)
   }
 
   property("foldRight") = forAll { (a: ofInt, z: List[Int]) =>
-    a.foldRight(z)(_ :: _) == a.self.foldRight(z)(_ :: _)
+    a.foldRight(z)(_ :: _) must_== a.self.foldRight(z)(_ :: _)
   }
 
   property("indexOf") = forAll { (a: ofInt, z: Int) =>
-    a.indexOf(z) == Option(a.self.indexOf(z)).filter(_ >= 0)
+    a.indexOf(z) must_== Option(a.self.indexOf(z)).filter(_ >= 0)
   }
 
   property("lastIndexOf") = forAll { (a: ofInt, z: Int) =>
-    a.lastIndexOf(z) == Option(a.self.lastIndexOf(z)).filter(_ >= 0)
+    a.lastIndexOf(z) must_== Option(a.self.lastIndexOf(z)).filter(_ >= 0)
   }
 
   property("mkString") = forAll { (a: ofInt, start: String, sep: String, end: String) =>
-    a.mkString(start, sep, end) == a.self.mkString(start, sep, end)
+    a.mkString(start, sep, end) must_== a.self.mkString(start, sep, end)
   }
 
   property("tails") = forAll { (a: ofInt) =>
-    a.tails.map(_.self.toSeq).toList == a.self.tails.map(_.toSeq).toList
+    a.tails.map(_.self.toSeq).toList must_== a.self.tails.map(_.toSeq).toList
   }
 
   property("inits") = forAll { (a: ofInt) =>
-    a.inits.map(_.self.toSeq).toList == a.self.inits.map(_.toSeq).toList
+    a.inits.map(_.self.toSeq).toList must_== a.self.inits.map(_.toSeq).toList
   }
 
   property("grouped") = forAll { (a: ofInt, n: Int) =>
