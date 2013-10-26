17bc8d2c19d45f3009bb3a599a2c4900be98ed01
Christian Krause <kizkizzbangbang@googlemail.com>
2013-10-26 22:44:44 +0200

    ReverseMapBenchmark for ofInt

---
+++ src/test/scala/ReverseMapBenchmark.scala
@@ -0,0 +1,28 @@
+package nobox
+package benchmark
+
+import org.scalameter.api._
+
+object ReverseBenchmark extends NoboxBenchmark {
+  performance of "scala.Array[Int]" in {
+    measure method "reverse.map" in {
+      using(arInts) curve("map") in {
+        a ⇒ a.reverse.map(_ + 1)
+      }
+    }
+
+    measure method "reverseMap" in {
+      using(arInts) curve("map") in {
+        a ⇒ a.reverseMap(_ + 1)
+      }
+    }
+  }
+
+  performance of "nobox.ofInt" in {
+    measure method "reverseMapInt" in {
+      using(ofInts) curve("map") in {
+        a ⇒ a.reverseMapInt(_ + 1)
+      }
+    }
+  }
+}
