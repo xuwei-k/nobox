b10a710174e0dc8e56b3a5066f328b04aa7503a3
xuwei-k <6b656e6a69@gmail.com>
2013-10-29 03:24:49 +0900

    wip RefBenchmark

--- src/test/scala/RefBenchmark.scala
+++ src/test/scala/RefBenchmark.scala
@@ -2,7 +2,7 @@ package nobox
 
 object RefBenchmark extends Benchmark{
 
-  def defaultSize = 5000000
+  def defaultSize = 10000000
   type Array1 = Array[Integer]
   type Array2 = ofRef[Integer]
   def createSampleArray(size: Int) = {
@@ -14,6 +14,46 @@ object RefBenchmark extends Benchmark{
   def run(){
     val a = args
     import a._
+
+    benchmark("foreach")(_.foreach(_ + 1), _.foreach(_ + 1))
+
+    benchmark("map")(_.map(_ + 1), _.map(_ + 1))
+
+    benchmark("reverseMap")(_.reverse.map(_ + 1), _.reverseMap(_ + 1))
+
+    benchmark("reverseMap")(_.reverseMap(_ + 1), _.reverseMap(_ + 1))
+
+    benchmark("exists")(_.exists(_ == -1), _.exists(_ == -1))
+
+    benchmark("contains")(_.contains(-1), _.contains(-1))
+
+    benchmark("filter")(_.filter(_ % 2 == 0), _.filter(_ % 2 == 0))
+
+    benchmark("find")(_.find(_ == -1), _.find(_ == -1))
+
+    benchmark("reverse")(_.reverse, _.reverse)
+
+    benchmark("flatMap")(_.flatMap(n => Array(n, n, n)), _.flatMap(n => Array(n, n, n)))
+
+    benchmark("collect")(
+      _.collect{case n if n > 10 => n + 1},
+      _.collect{case n if n > 10 => n + 1}
+    )
+
+    val IntOne = Integer.valueOf(1)
+
+    benchmark("collectFirst")(
+      _.collectFirst{case IntOne => 2},
+      _.collectFirstInt{case IntOne => 2}
+    )
+
+    benchmark("takeWhile")(_.takeWhile(_ > 1), _.takeWhile(_ > 1))
+
+    benchmark("take")(_.take(size / 2), _.take(size / 2))
+
+    benchmark("takeRight")(_.takeRight(size / 2), _.takeRight(size / 2))
+
+    benchmark("splitAt")(_.splitAt((size / 1.5).toInt), _.splitAt((size / 1.5).toInt))
 
     benchmark("scanRight")(
       _.scanRight(List[Integer](0))(_ :: _),
