9f643fc3e4da12d74bcb7996258c565b85369820
xuwei-k <6b656e6a69@gmail.com>
2013-10-31 12:38:34 +0900

    add travis test-only

--- travis.sh
+++ travis.sh
@@ -1,8 +1,12 @@
-if [[ $TRAVIS_SCALA_VERSION != "2.11.0-M6" ]]; then
-  sbt ++$TRAVIS_SCALA_VERSION printInfo compile 'test:runMain nobox.Info all' test benchmark scalameter/test checkPackage
-else
+#!/bin/bash
+
+if [[ $TRAVIS_SCALA_VERSION = "2.11.0-M6" ]]; then
   sbt generator/generateSources printInfo && rm generate.sbt &&
   sbt ++$TRAVIS_SCALA_VERSION nobox/compile 'nobox/test:runMain nobox.Info all' nobox/test nobox/benchmark checkPackage
+elif [[ $TRAVIS_SCALA_VERSION = "test-only" ]]; then
+  sbt '++ 2.10.3' compile test
+else
+  sbt ++$TRAVIS_SCALA_VERSION printInfo compile 'test:runMain nobox.Info all' test benchmark scalameter/test checkPackage
 fi
 
 
