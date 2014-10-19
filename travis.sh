205ae55980fe53e9363574ea98c60c8f3838eb80
xuwei-k <6b656e6a69@gmail.com>
2014-10-20 07:33:54 +0900

    Revert "Scala 2.11.3"
    https://groups.google.com/d/topic/scala-internals/SSD9BNJaFbU/discussion
    
    This reverts commit 9aed024cbe107a6bb29d17733e5cf5a0de661e8e.

--- travis.sh
+++ travis.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $TRAVIS_SCALA_VERSION = "ref-211" ]]; then
-  sbt '++ 2.11.3' 'benchmark RefBenchmark'
+  sbt '++ 2.11.2' 'benchmark RefBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "int-211" ]]; then
-  sbt '++ 2.11.3' 'benchmark IntBenchmark'
+  sbt '++ 2.11.2' 'benchmark IntBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "ref-210" ]]; then
   sbt '++ 2.10.4' 'benchmark RefBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "int-210" ]]; then
   sbt '++ 2.10.4' 'benchmark IntBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "test-only-211" ]]; then
-  sbt '++ 2.11.3' test $(if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then echo publish ; fi)
+  sbt '++ 2.11.2' test $(if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then echo publish ; fi)
 elif [[ $TRAVIS_SCALA_VERSION = "test-only-210" ]]; then
   sbt '++ 2.10.4' test $(if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then echo publish ; fi)
 elif [[ $TRAVIS_SCALA_VERSION = "scalameter" ]]; then
