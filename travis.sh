4928692b19829878957ed0a62138168d91232e6b
xuwei-k <6b656e6a69@gmail.com>
2015-03-03 15:48:29 +0900

    Scala 2.11.6

--- travis.sh
+++ travis.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ $TRAVIS_SCALA_VERSION = "ref-211" ]]; then
-  sbt -Ddisable_sxr '++ 2.11.4' 'benchmark RefBenchmark'
+  sbt -Ddisable_sxr '++ 2.11.6' 'benchmark RefBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "int-211" ]]; then
-  sbt -Ddisable_sxr '++ 2.11.4' 'benchmark IntBenchmark'
+  sbt -Ddisable_sxr '++ 2.11.6' 'benchmark IntBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "ref-210" ]]; then
   sbt -Ddisable_sxr '++ 2.10.4' 'benchmark RefBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "int-210" ]]; then
   sbt -Ddisable_sxr '++ 2.10.4' 'benchmark IntBenchmark'
 elif [[ $TRAVIS_SCALA_VERSION = "test-only-211" ]]; then
-  sbt '++ 2.11.4' test $(if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then echo publish ; fi)
+  sbt '++ 2.11.6' test $(if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then echo publish ; fi)
 elif [[ $TRAVIS_SCALA_VERSION = "test-only-210" ]]; then
   sbt '++ 2.10.4' test $(if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then echo publish ; fi)
 elif [[ $TRAVIS_SCALA_VERSION = "scalameter" ]]; then
