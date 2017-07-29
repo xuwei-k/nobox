c0253e90e00e3edd24a4d5307a936596f70f469b
xuwei-k <6b656e6a69@gmail.com>
2017-07-29 16:48:56 +0900

    Scala 2.13.0-M2

--- project/common.scala
+++ project/common.scala
@@ -64,7 +64,7 @@ object Common {
 
   val commonSettings: Seq[Def.Setting[_]] = Seq(
     scalaVersion := Scala211,
-    crossScalaVersions := "2.13.0-M1" :: "2.12.3" :: Scala211 :: Nil,
+    crossScalaVersions := "2.13.0-M2" :: "2.12.3" :: Scala211 :: Nil,
     organization := "com.github.xuwei-k",
     commands += Command.command("updateReadme")(updateReadme),
     resolvers += Opts.resolver.sonatypeReleases,
