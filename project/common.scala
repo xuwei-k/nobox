1ce54068797d373883b0db0e8a7cb8a9ccea19b1
kenji yoshida <6b656e6a69@gmail.com>
2021-03-30 11:18:37 +0900

    Scala 3.0.0-RC2

--- project/common.scala
+++ project/common.scala
@@ -65,7 +65,7 @@ object Common {
 
   val commonSettings: Seq[Def.Setting[_]] = Seq(
     scalaVersion := Scala211,
-    crossScalaVersions := "3.0.0-RC1" :: "2.13.5" :: "2.12.13" :: Scala211 :: Nil,
+    crossScalaVersions := "3.0.0-RC2" :: "2.13.5" :: "2.12.13" :: Scala211 :: Nil,
     organization := "com.github.xuwei-k",
     commands += Command.command("updateReadme")(updateReadme),
     publishTo := sonatypePublishToBundle.value,
