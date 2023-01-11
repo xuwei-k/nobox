640721d3de26d3d41b22f561816fa038fbe28343
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2023-01-11 17:34:07 +0000

    Update scala3-library, ... to 3.2.2

--- project/common.scala
+++ project/common.scala
@@ -64,7 +64,7 @@ object Common {
 
   val commonSettings: Seq[Def.Setting[_]] = Seq(
     scalaVersion := Scala211,
-    crossScalaVersions := "3.2.1" :: "2.13.10" :: "2.12.17" :: Scala211 :: Nil,
+    crossScalaVersions := "3.2.2" :: "2.13.10" :: "2.12.17" :: Scala211 :: Nil,
     organization := "com.github.xuwei-k",
     commands += Command.command("updateReadme")(updateReadme),
     publishTo := sonatypePublishToBundle.value,
